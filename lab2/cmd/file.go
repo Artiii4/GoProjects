b60c0098240cf5eca81b1b2c52083233d164fd25
Artiii4 <112716998+Artiii4@users.noreply.github.com>
2023-10-26 15:07:35 +0300

    with QuickSort file.go

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,8 +1,25 @@
 package main
 
-import "sort"
-
 func findKthLargest(array []int, neededNum int) int {
-	sort.Ints(array)
-	return array[len(array)-neededNum]
+	return QuickSort(array)[neededNum-1]
+}
+
+func QuickSort(array []int) []int {
+	lenArr := len(array)
+	if lenArr < 2 {
+		return array
+	}
+	supEl := array[0]
+	var leftPart []int
+	var rightPart []int
+	for i := 1; i < lenArr; i++ {
+		if supEl < array[i] {
+			leftPart = append(leftPart, array[i])
+		} else {
+			rightPart = append(rightPart, array[i])
+		}
+	}
+	sortedArray := append(QuickSort(leftPart), supEl)
+	sortedArray = append(sortedArray, QuickSort(rightPart)...)
+	return sortedArray
 }
