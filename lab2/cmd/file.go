4f4a6d9348691785e0ca49c34ee4d8588a329376
Artiii4 <112716998+Artiii4@users.noreply.github.com>
2023-10-26 22:19:54 +0300

    Update file.go

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,16 +1,10 @@
 package main
 
-func findKthLargest(nums []int, k int) int {
-	if len(nums) < k || k <= 0 {
-		return -1
-	}
-
+func findKthLargest(array []int, k int) int {
 	left := 0
-	right := len(nums) - 1
-
+	right := len(array) - 1
 	for {
-		pivotIndex := partition(nums, left, right)
-
+		pivotIndex := partition(array, left, right)
 		if pivotIndex == k-1 {
 			return nums[pivotIndex]
 		} else if pivotIndex > k-1 {
@@ -24,15 +18,12 @@ func findKthLargest(nums []int, k int) int {
 func partition(nums []int, left, right int) int {
 	pivot := nums[right]
 	pivotIndex := left
-
 	for i := left; i < right; i++ {
 		if nums[i] > pivot {
 			nums[i], nums[pivotIndex] = nums[pivotIndex], nums[i]
 			pivotIndex++
 		}
 	}
-
 	nums[pivotIndex], nums[right] = nums[right], nums[pivotIndex]
-
 	return pivotIndex
 }
