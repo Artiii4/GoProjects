d031f9e5b25e34ba9e0aff3a4cebd6c2c4c291cf
Artiii4 <112716998+Artiii4@users.noreply.github.com>
2023-10-26 11:51:49 +0300

    Update file.go

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,12 +1,6 @@
 package main
 
 func findKthLargest(array []int, neededNum int) int {
-	for i := 0; i < (len(array) - 1); i++ {
-		for j := i + 1; j < len(array); j++ {
-			if array[i] < array[j] {
-				array[i], array[j] = array[j], array[i]
-			}
-		}
-	}
-	return array[neededNum-1]
+	sort.Ints(array)
+	return array[len(array)-neededNum]
 }
