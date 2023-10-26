7224978609fb527de2fb733e7b159801c2c844bd
Artiii4 <112716998+Artiii4@users.noreply.github.com>
2023-10-26 11:45:43 +0300

    Update file.go

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,32 +1,12 @@
 package main
 
 func findKthLargest(array []int, neededNum int) int {
-	return mergeSort(array)[neededNum-1]
-}
-
-func merge(leftPart []int, rightPart []int) []int {
-	var mergedArray []int
-	for len(leftPart) > 0 && len(rightPart) > 0 {
-		if leftPart[0] > rightPart[0] {
-			mergedArray = append(mergedArray, leftPart[0])
-			leftPart = leftPart[1:]
-		} else {
-			mergedArray = append(mergedArray, rightPart[0])
-			rightPart = rightPart[1:]
+	for i := 0; i < (len(array) - 1); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] < array[j] {
+				array[i], array[j] = array[j], array[i]
+			}
 		}
 	}
-	mergedArray = append(mergedArray, leftPart...)
-	mergedArray = append(mergedArray, rightPart...)
-	return mergedArray
-}
-
-func mergeSort(array []int) []int {
-	len := len(array)
-	if len < 2 {
-		return array
-	}
-	middle := len / 2
-	leftPart := mergeSort(array[:middle])
-	rightPart := mergeSort(array[middle:])
-	return merge(leftPart, rightPart)
+	return array[neededNum-1]
 }
